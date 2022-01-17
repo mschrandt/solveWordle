1af0882afa10d9b7d1d5d8f99729cd74da059227
Michael Schrandt <mikeschrandt@gmail.com>
2022-01-17 09:44:57 -0600

    add global picked list

--- solveWordle.py
+++ solveWordle.py
@@ -1,9 +1,12 @@
 import random
+import math
 
 def load():
     acceptedWords = []
     possibleWords = []
     letterFrequency = {}
+    global pickClues
+    pickClues = []
 
     with open("possibleWords.txt", "r", newline='') as wordFile:
         for word in wordFile:
@@ -29,17 +32,27 @@ def load():
     return acceptedWords, possibleWords, letterFrequency
 
 def filterWords(acceptedWords, possibleWords, letterFrequency, guess, guessResults):
+    global pickClues
     if guessResults == "":
         words.remove(guess)
-        return words, letterFrequency
+        return acceptedWords, possibleWords, letterFrequency
 
     newPossibleWords = [x for x in possibleWords if matchesGuess(x, guess, guessResults)]
-    newAcceptedWords = [x for x in acceptedWords if matchesGuess(x, guess, guessResults)]
+    newAcceptedWords =  [x for x in acceptedWords if x != guess]# matchesGuess(x, guess, guessResults)]
     newLetterFrequency = {}
+
+    newPickClues = []
+    for i in range(len(guessResults)):
+        if guessResults[i] != 'x':
+            if guess[i] in pickClues:
+                pickClues.remove(guess[i])
+            newPickClues.append(guess[i])
+        newPickClues += pickClues
+    pickClues = newPickClues
+
     for word in newPossibleWords:
-        for i in range(len(guessResults)):
-            if guessResults[i] != 'x':
-                word.replace(guess[i],'',1)
+        for letter in pickClues:
+            word = word.replace(letter,'',1)
         picked = set()
         for c in word:
             if c not in newLetterFrequency:
@@ -50,6 +63,9 @@ def filterWords(acceptedWords, possibleWords, letterFrequency, guess, guessResul
     return newAcceptedWords, newPossibleWords, newLetterFrequency
 
 def matchesGuess(word, guess, guessResults):
+    if word == guess:
+        return False
+
     for i in range(len(word)):
         if guessResults[i] == 'g' and word[i] != guess[i]:
             return False
@@ -88,32 +104,42 @@ def findLetterIndexesInWord(word, letter):
     return [i for i, ltr in enumerate(word) if ltr == letter]
 
 def pickWord(acceptedWords, possibleWords, letterFrequency, lastGuess, lastGuessResult):
-    highestScore = -1
+    highestScore = 0#999
     bestWord = ''
+    neverPickedWord = True
 
+    if len(possibleWords) == 1:
+        return possibleWords[0]
     for word in acceptedWords:
         wordCopy = word
         for i in range(len(lastGuessResult)):
             if lastGuessResult[i] != 'x':
-                word.replace(lastGuess[i],'',1)
+                word = word.replace(lastGuess[i],'',1)
 
         frequencySum = 0
         picked = set()
         for letter in word:
             if letter not in picked:
-                if letter in letterFrequency:
-                    frequencySum += letterFrequency[letter]
+                if letter in letterFrequency and letterFrequency[letter] != 0:
+                    frequencySum += letterFrequency[letter]#abs(0.5 - letterFrequency[letter]/len(acceptedWords))
+                else:
+                    frequencySum += 0#0.5
                 picked.add(letter)
-                #frequencySum += letterFrequency[letter] * 0.5
+            else:
+                frequencySum += 0#0.5
 
         if frequencySum > highestScore:
+            neverPickedWord = False
             highestScore = frequencySum
             bestWord = wordCopy
 
+    if neverPickedWord:
+        bestWord = random.choice(possibleWords)
     return bestWord
 
 def evaluateGuess(word, guess):
     result = ""
+    usedIndexes = set()
     for i in range(len(word)):
         if word[i] == guess[i]:
             result += 'g'
@@ -121,7 +147,8 @@ def evaluateGuess(word, guess):
             foundLetter = False
             foundIndex = word.find(guess[i])
             while foundIndex != -1:
-                if guess[foundIndex] != word[foundIndex]:
+                if guess[foundIndex] != word[foundIndex] and foundIndex not in usedIndexes:
+                    usedIndexes.add(foundIndex)
                     foundLetter=True
                     break
                 foundIndex = word.find(guess[i], foundIndex+1)
@@ -142,8 +169,9 @@ def play():
     print(len(possibleWords)," words in dictionary.")
 
     guessResults = ""
-    while guessResults != 'ggggg' and len(words) > 0:
-        currentGuess = pickWord(acceptedWords, possibleWords, letterFrequency)
+    currentGuess = ""
+    while guessResults != 'ggggg' and len(possibleWords) > 0:
+        currentGuess = pickWord(acceptedWords, possibleWords, letterFrequency, currentGuess, guessResults)
         print("Best guess:",currentGuess)
         print("Guess results (gyxgx, blank to get a new word, or word<space>gyxgx for custom guess)")
         guessResults = input(":").lower()
@@ -159,27 +187,28 @@ def play():
 def playAi(showPlay = True):
     acceptedWords, possibleWords, letterFrequency = load()
     word = random.choice(possibleWords)
-    #word = "sauce"
+    #word = "corny"
     print("Word is",word)
     if showPlay:
-        print("Letter frequencies")
-        for letter in letterFrequency:
-            print(letter + ":", letterFrequency[letter])
-
         print(len(possibleWords)," words in dictionary.")
-        print("Word is:", word)
 
     guessResults = ""
     currentGuess = ""
     numGuesses = 0
+    guessed = False
     while guessResults != 'ggggg' and len(possibleWords) > 0:
+
         currentGuess = pickWord(acceptedWords, possibleWords, letterFrequency, currentGuess, guessResults)
         #if(numGuesses == 0):
         #    currentGuess = "roate"
         if showPlay:
+            for letter in letterFrequency:
+                print(letter + ":", letterFrequency[letter])
             print("Best guess:",currentGuess)
             print("Guess results (gyxgx, blank to get a new word, or word<space>gyxgx for custom guess)")
 
+        if currentGuess == word:
+            guessed = True
         guessResults = evaluateGuess(word, currentGuess)
         numGuesses += 1
 
@@ -192,7 +221,7 @@ def playAi(showPlay = True):
             print(len(possibleWords)," words remaining in dictionary.")
 
 
-    if len(possibleWords) == 0:
+    if not guessed:
         print("Failed to guess word:", word)
     else:
         print("Guessed", word, "in", numGuesses,"guesses")
@@ -200,7 +229,9 @@ def playAi(showPlay = True):
 
 totGuesses = 0
 numGames = 100
-for _ in range(numGames):
-    totGuesses += playAi(False)
+#for _ in range(numGames):
+#    totGuesses += playAi(False)
 
-print(totGuesses/numGames)
+#print(totGuesses/numGames)
+
+play()
